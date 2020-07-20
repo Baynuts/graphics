5dd356bb5cf0c920ea534273d02fce50d17eff71
D Bailey <github@baileygb.com>
2020-07-20 19:36:01 +0000

    Add Bootstrap toasts to template

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -10,6 +12,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     customtext = None
@@ -31,13 +34,14 @@ def add_to_cart(request, item_id):
             cart[item_id] += quantity
         else:
             cart[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
 
 
     request.session['cart'] = cart
     return redirect(redirect_url)    
 
-def adjust_bag(request, item_id):
+def adjust_cart(request, item_id):
     """ADD SOMETHING HERE"""
 
     quantity = int(request.POST.get('quantity'))
