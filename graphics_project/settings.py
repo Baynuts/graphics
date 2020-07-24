b21750d306487cdba0f4deaa5124efc6ebc6631f
D Bailey <github@baileygb.com>
2020-07-24 19:20:22 +0000

    Add Django Crispy Forms with bootstrap4 templates

--- graphics_project/settings.py
+++ graphics_project/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'products',
-    'cart'
+    'cart',
+    'checkout',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -57,6 +59,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'graphics_project.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -75,6 +79,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
