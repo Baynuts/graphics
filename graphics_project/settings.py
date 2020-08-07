4f36131c94f3c14f8a3ba5f7d10ee2c3336a922d
D Bailey <github@baileygb.com>
2020-08-07 15:56:41 +0000

    Redesign navigation bar

--- graphics_project/settings.py
+++ graphics_project/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '' )
+SECRET_KEY = os.environ.get('SECRET_KEY', 'byuibyuiuyuhu7y78y7hyuku' )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
-
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = 'TRUE'
 ALLOWED_HOSTS = ['localhost', 'graphicz-drb.herokuapp.com']
 
 
