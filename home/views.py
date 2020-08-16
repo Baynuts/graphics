23a0d1629469ee9fd2be137e298cb155065e56a7
D Bailey <github@baileygb.com>
2020-08-16 18:13:26 +0000

    Added reasons to buy, product links on homepage, tidied index code

--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
+from products.models import Product, Category
 
 # Create your views here.
 
